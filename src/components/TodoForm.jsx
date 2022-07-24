ab0ebf062063713fdb87de806e31c75c6cbf532e
Ambika kumar <95169962+Ambika2510@users.noreply.github.com>
2022-07-24 19:57:56 +0530

    update

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -3,8 +3,23 @@ import Todolist from './Todolist';
 import'./TodoForm.css';
 
 function TodoForm(props) {
+        let put=[];
+        let tt=[];
+  
+    if(localStorage.length>0){
+      put= JSON.parse(localStorage.getItem("tt"));
+      for(let i=0;i<put.length;i++){
+        tt.push(put[i].text);
+      }
+    }
+
+  
     const [input,setInput]=useState("");
-    const [items,setitems]=useState([]);
+    let [items,setitems]=useState(tt);
+    
+   
+    
+    
     const handleChange= e =>{
         setInput(e.target.value);
     }
@@ -14,6 +29,11 @@ function TodoForm(props) {
     }
     const it=[...items,input];
     setitems(it);
+    let data=[];
+    for(let i=0;i<it.length;i++){
+         data.push({id:i,text:it[i]});
+    }
+    localStorage.setItem("tt", JSON.stringify(data));
     setInput("");
    }
    const deleteitems=(id)=>{
@@ -21,17 +41,27 @@ function TodoForm(props) {
               return i!==id;
     })
     setitems(it);
+    localStorage.clear();
+    let data=[];
+    for(let i=0;i<it.length;i++){
+         data.push({id:i,text:it[i]});
+    }
+    localStorage.setItem("tt", JSON.stringify(data));
 
    }
+
+     
   return (
     <div className='main-div'>
       <div className='centre-div'>
         <h1>To Do List</h1>
  <input type="text" placeholder='Add a todo' value={input}  name='text' className='todo-input' onChange={handleChange}/>
   <button className='todo-button' onClick={listofitems}>+</button>
+
     {items.map((e,i)=>{
       return <Todolist key={i} id={i} text={e} onselect={deleteitems}/>;
     })}
+     
   
     </div>
     </div>
